f829edf815d9ef677aa87077889dfc9d884296d4
Philip Neustrom <philipn@gmail.com>
2012-09-13 18:31:34 -0700

    rm redundant license messages at top of files.

--- eav/validators.py
+++ eav/validators.py
@@ -1,25 +1,4 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
-#
-#    This software is derived from EAV-Django originally written and
-#    copyrighted by Andrey Mikhaylenko <http://pypi.python.org/pypi/eav-django>
-#
-#    This is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This software is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with EAV-Django.  If not, see <http://gnu.org/licenses/>.
-'''
-**********
-validators
-**********
+"""
 This module contains a validator for each Attribute datatype.
 
 A validator is a callable that takes a value and raises a ``ValidationError``
@@ -32,7 +11,7 @@ These validators are called by the
 
 Functions
 ---------
-'''
+"""
 
 from datetime import datetime, date
 
