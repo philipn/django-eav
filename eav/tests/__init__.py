35573f3591b47328689028f6cb083edc42d3c26d
Mike Ivanov <mivanov@gmail.com>
2012-10-02 14:02:39 -0700

    Fixed limiting attributes tests

--- eav/tests/__init__.py
+++ eav/tests/__init__.py
@@ -8,7 +8,7 @@ INSTALLED_APPS.append('eav.tests')
 mgr.set(INSTALLED_APPS=INSTALLED_APPS)
 
 from .registry import *
-#from .limiting_attributes import *
+from .limiting_attributes import *
 #from .data_validation import *
 #from .misc_models import *
 #from .queries import *
--- eav/tests/limiting_attributes.py
+++ eav/tests/limiting_attributes.py
@@ -2,30 +2,33 @@ from django.test import TestCase
 
 import eav
 from ..registry import EavConfig
-from ..models import Attribute, Value
 
-from .models import Patient, Encounter
+from .models import Patient, PatientAttribute, PatientValue,\
+                    Encounter, EncounterAttribute, EncounterValue
 
 
 class LimitingAttributes(TestCase):
 
     def setUp(self):
         class EncounterEavConfig(EavConfig):
-            manager_attr = 'eav_objects'
             eav_attr = 'eav_field'
-            generic_relation_attr = 'encounter_eav_values'
-            generic_relation_related_name = 'encounters'
+            eav_relation_attr = 'encounter_eav_values'
 
             @classmethod
             def get_attributes(cls, entity=None):
-                return Attribute.objects.filter(slug__contains='a')
+                return EncounterAttribute.objects.filter(slug__contains='a')
 
-        eav.register(Encounter, EncounterEavConfig)
-        eav.register(Patient)
+        eav.register(Encounter, EncounterAttribute, EncounterValue,
+                     EncounterEavConfig)
+        eav.register(Patient, PatientAttribute, PatientValue)
 
-        Attribute.objects.create(name='age', datatype=Attribute.TYPE_INT)
-        Attribute.objects.create(name='height', datatype=Attribute.TYPE_FLOAT)
-        Attribute.objects.create(name='weight', datatype=Attribute.TYPE_FLOAT)
+        PatientAttribute.objects.create(name='age', datatype=PatientAttribute.TYPE_INT)
+        PatientAttribute.objects.create(name='height', datatype=PatientAttribute.TYPE_FLOAT)
+        PatientAttribute.objects.create(name='weight', datatype=PatientAttribute.TYPE_FLOAT)
+
+        EncounterAttribute.objects.create(name='age', datatype=EncounterAttribute.TYPE_INT)
+        EncounterAttribute.objects.create(name='height', datatype=EncounterAttribute.TYPE_FLOAT)
+        EncounterAttribute.objects.create(name='weight', datatype=EncounterAttribute.TYPE_FLOAT)
 
     def tearDown(self):
         eav.unregister(Encounter)
@@ -43,39 +46,14 @@ class LimitingAttributes(TestCase):
         p.eav.age = 3
         p.eav.height = 2.3
         p.save()
+        self.assertEqual(PatientValue.objects.count(), 2)
         e.eav_field.age = 4
         e.eav_field.height = 4.5
         e.save()
-        self.assertEqual(Value.objects.count(), 3)
+        self.assertEqual(EncounterValue.objects.count(), 1)
         p = Patient.objects.get(name='Jon')
         self.assertEqual(p.eav.age, 3)
         self.assertEqual(p.eav.height, 2.3)
         e = Encounter.objects.get(num=1)
         self.assertEqual(e.eav_field.age, 4)
         self.assertFalse(hasattr(e.eav_field, 'height'))
-        
-        
-class AttributesWithParents(TestCase):
-    
-    def setUp(self):
-        eav.register(Encounter, filter_by_parent=True)
-        eav.register(Patient, filter_by_parent=True)
-        Attribute.objects.create(name='age', parent=Patient, datatype=Attribute.TYPE_INT)
-        Attribute.objects.create(name='date', parent=Encounter, datatype=Attribute.TYPE_DATE)
-        Attribute.objects.create(name='cost', parent=Encounter, datatype=Attribute.TYPE_FLOAT)
-    
-    def tearDown(self):
-        eav.unregister(Encounter)
-        eav.unregister(Patient)
-    
-    def test_partitioned_admin(self):
-        """
-        Tests of the attribute partitioning admin objects.
-        """
-        patient_attrs = Patient._eav_config_cls.get_attributes()
-        self.assertEqual(len(patient_attrs), 1)
-        
-        encounter_attrs = Encounter._eav_config_cls.get_attributes()
-        self.assertEqual(len(encounter_attrs), 2)
-
-        
