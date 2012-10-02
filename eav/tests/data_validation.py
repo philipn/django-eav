7c0b9eabe82db53fd451ddf8cb891b5ebb33e1bf
Mike Ivanov <mivanov@gmail.com>
2012-10-02 14:28:24 -0700

    Fix data validation tests

--- eav/tests/data_validation.py
+++ eav/tests/data_validation.py
@@ -41,113 +41,100 @@ class DataValidation(TestCase):
         b.save()
         a.datatype = PatientAttribute.TYPE_INT
         self.assertRaises(ValidationError, a.save)
+
+    def test_int_validation(self):
+        p = Patient.objects.create(name='Joe')
+        p.eav.age = 'bad'
+        self.assertRaises(ValidationError, p.save)
+        p.eav.age = 15
+        p.save()
+        self.assertEqual(Patient.objects.get(pk=p.pk).eav.age, 15)
+
+    def test_date_validation(self):
+        p = Patient.objects.create(name='Joe')
+        p.eav.dob = 'bad'
+        self.assertRaises(ValidationError, p.save)
+        p.eav.dob = 15
+        self.assertRaises(ValidationError, p.save)
+        now_datetime = now()
+        p.eav.dob = now_datetime
+        p.save()
+        self.assertEqual(Patient.objects.get(pk=p.pk).eav.dob, now_datetime)
+        self.assertEqual(Patient.objects.get(pk=p.pk).eav.dob.date(), now_datetime.date())
+
+    def test_float_validation(self):
+        p = Patient.objects.create(name='Joe')
+        p.eav.height = 'bad'
+        self.assertRaises(ValidationError, p.save)
+        p.eav.height = 15
+        p.save()
+        self.assertEqual(Patient.objects.get(pk=p.pk).eav.height, 15)
+        p.eav.height='2.3'
+        p.save()
+        self.assertEqual(Patient.objects.get(pk=p.pk).eav.height, 2.3)
+
+    def test_text_validation(self):
+        p = Patient.objects.create(name='Joe')
+        p.eav.city = 5
+        self.assertRaises(ValidationError, p.save)
+        p.eav.city = 'El Dorado'
+        p.save()
+        self.assertEqual(Patient.objects.get(pk=p.pk).eav.city, 'El Dorado')
+
+    def test_bool_validation(self):
+        p = Patient.objects.create(name='Joe')
+        p.eav.pregnant = 5
+        self.assertRaises(ValidationError, p.save)
+        p.eav.pregnant = True
+        p.save()
+        self.assertEqual(Patient.objects.get(pk=p.pk).eav.pregnant, True)
+
+    def test_enum_validation(self):
+        yes = EnumValue.objects.create(value='yes')
+        no = EnumValue.objects.create(value='no')
+        unkown = EnumValue.objects.create(value='unkown')
+        green = EnumValue.objects.create(value='green')
+        ynu = EnumGroup.objects.create(name='Yes / No / Unknown')
+        ynu.enums.add(yes)
+        ynu.enums.add(no)
+        ynu.enums.add(unkown)
+        PatientAttribute.objects.create(name='Fever?', datatype=PatientAttribute.TYPE_ENUM, enum_group=ynu)
+
+        p = Patient.objects.create(name='Joe')
+        p.eav.fever = 5
+        self.assertRaises(ValidationError, p.save)
+        p.eav.fever = object
+        self.assertRaises(ValidationError, p.save)
+        p.eav.fever = 'yes'
+        self.assertRaises(ValidationError, p.save)
+        p.eav.fever = green
+        self.assertRaises(ValidationError, p.save)
+        p.eav.fever = EnumValue(value='yes')
+        self.assertRaises(ValidationError, p.save)
+        p.eav.fever = no
+        p.save()
+        self.assertEqual(Patient.objects.get(pk=p.pk).eav.fever, no)
 #
-#    def test_int_validation(self):
-#        p = Patient.objects.create(name='Joe')
-#        p.eav.age = 'bad'
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.age = 15
-#        p.save()
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.age, 15)
-#
-#    def test_date_validation(self):
-#        p = Patient.objects.create(name='Joe')
-#        p.eav.dob = 'bad'
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.dob = 15
-#        self.assertRaises(ValidationError, p.save)
-#        now_datetime = now()
-#        p.eav.dob = now_datetime
-#        p.save()
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.dob, now_datetime)
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.dob.date(), now_datetime.date())
-#
-#    def test_float_validation(self):
-#        p = Patient.objects.create(name='Joe')
-#        p.eav.height = 'bad'
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.height = 15
-#        p.save()
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.height, 15)
-#        p.eav.height='2.3'
-#        p.save()
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.height, 2.3)
-#
-#    def test_text_validation(self):
-#        p = Patient.objects.create(name='Joe')
-#        p.eav.city = 5
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.city = 'El Dorado'
-#        p.save()
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.city, 'El Dorado')
-#
-#    def test_bool_validation(self):
-#        p = Patient.objects.create(name='Joe')
-#        p.eav.pregnant = 5
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.pregnant = True
-#        p.save()
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.pregnant, True)
-#
-#    def test_object_validation(self):
-#        p = Patient.objects.create(name='Joe')
-#        p.eav.user = 5
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.user = object
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.user = User(username='joe')
-#        self.assertRaises(ValidationError, p.save)
-#        u = User.objects.create(username='joe', email='joe@example.com')
-#        p.eav.user = u
-#        p.save()
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.user, u)
-#
-#    def test_enum_validation(self):
-#        yes = EnumValue.objects.create(value='yes')
-#        no = EnumValue.objects.create(value='no')
-#        unkown = EnumValue.objects.create(value='unkown')
-#        green = EnumValue.objects.create(value='green')
-#        ynu = EnumGroup.objects.create(name='Yes / No / Unknown')
-#        ynu.enums.add(yes)
-#        ynu.enums.add(no)
-#        ynu.enums.add(unkown)
-#        Attribute.objects.create(name='Fever?', datatype=Attribute.TYPE_ENUM, enum_group=ynu)
-#
-#        p = Patient.objects.create(name='Joe')
-#        p.eav.fever = 5
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.fever = object
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.fever = 'yes'
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.fever = green
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.fever = EnumValue(value='yes')
-#        self.assertRaises(ValidationError, p.save)
-#        p.eav.fever = no
-#        p.save()
-#        self.assertEqual(Patient.objects.get(pk=p.pk).eav.fever, no)
-#
-#    def test_enum_datatype_without_enum_group(self):
-#        a = Attribute(name='Age Bracket', datatype=Attribute.TYPE_ENUM)
-#        self.assertRaises(ValidationError, a.save)
-#        yes = EnumValue.objects.create(value='yes')
-#        no = EnumValue.objects.create(value='no')
-#        unkown = EnumValue.objects.create(value='unkown')
-#        ynu = EnumGroup.objects.create(name='Yes / No / Unknown')
-#        ynu.enums.add(yes)
-#        ynu.enums.add(no)
-#        ynu.enums.add(unkown)
-#        a = Attribute(name='Age Bracket', datatype=Attribute.TYPE_ENUM, enum_group=ynu)
-#        a.save()
-#
-#    def test_enum_group_on_other_datatype(self):
-#        yes = EnumValue.objects.create(value='yes')
-#        no = EnumValue.objects.create(value='no')
-#        unkown = EnumValue.objects.create(value='unkown')
-#        ynu = EnumGroup.objects.create(name='Yes / No / Unknown')
-#        ynu.enums.add(yes)
-#        ynu.enums.add(no)
-#        ynu.enums.add(unkown)
-#        a = Attribute(name='color', datatype=Attribute.TYPE_TEXT, enum_group=ynu)
-#        self.assertRaises(ValidationError, a.save)
+    def test_enum_datatype_without_enum_group(self):
+        a = PatientAttribute(name='Age Bracket', datatype=PatientAttribute.TYPE_ENUM)
+        self.assertRaises(ValidationError, a.save)
+        yes = EnumValue.objects.create(value='yes')
+        no = EnumValue.objects.create(value='no')
+        unkown = EnumValue.objects.create(value='unkown')
+        ynu = EnumGroup.objects.create(name='Yes / No / Unknown')
+        ynu.enums.add(yes)
+        ynu.enums.add(no)
+        ynu.enums.add(unkown)
+        a = PatientAttribute(name='Age Bracket', datatype=PatientAttribute.TYPE_ENUM, enum_group=ynu)
+        a.save()
+
+    def test_enum_group_on_other_datatype(self):
+        yes = EnumValue.objects.create(value='yes')
+        no = EnumValue.objects.create(value='no')
+        unkown = EnumValue.objects.create(value='unkown')
+        ynu = EnumGroup.objects.create(name='Yes / No / Unknown')
+        ynu.enums.add(yes)
+        ynu.enums.add(no)
+        ynu.enums.add(unkown)
+        a = PatientAttribute(name='color', datatype=PatientAttribute.TYPE_TEXT, enum_group=ynu)
+        self.assertRaises(ValidationError, a.save)
